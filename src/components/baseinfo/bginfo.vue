d7b56c0286eb00aa7edec1316c38dafcc024163a
zhanghaiyan <zhanghaiyan@cloudh.net.cn>
2019-03-24 22:02:55 +0800

    【添加】修改基础信息页面数字增量框功能

--- src/components/baseinfo/bginfo.vue
+++ src/components/baseinfo/bginfo.vue
@@ -30,91 +30,34 @@
           <dl>
             <dt>{{$t('baseinfo.time.title')}}</dt>
             <dd>
-              <h2>
-                <ul>
-                  <li>2017{{$t('baseinfo.time.year')}}</li>
-                  <li class="show">2018{{$t('baseinfo.time.year')}}</li>
-                  <li>2019{{$t('baseinfo.time.year')}}</li>
-                </ul>
-                <i class="ylo_prev"></i>
-                <i class="ylo_next"></i>
-              </h2>
-              <h2>
-                <ul>
-                  <li>10{{$t('baseinfo.time.month')}}</li>
-                  <li class="show">11{{$t('baseinfo.time.month')}}</li>
-                  <li>12{{$t('baseinfo.time.month')}}</li>
-                </ul>
-                <i class="ylo_prev"></i>
-                <i class="ylo_next"></i>
-              </h2>
-              <h2>
-                <ul>
-                  <li>11{{$t('baseinfo.time.day')}}</li>
-                  <li>12{{$t('baseinfo.time.day')}}</li>
-                  <li>13{{$t('baseinfo.time.day')}}</li>
-                  <li class="show">14{{$t('baseinfo.time.day')}}</li>
-                  <li>15{{$t('baseinfo.time.day')}}</li>
-                  <li>16{{$t('baseinfo.time.day')}}</li>
-                  <li>17{{$t('baseinfo.time.day')}}</li>
-                </ul>
-                <i class="ylo_prev"></i>
-                <i class="ylo_next"></i>
-              </h2>
+              <incretInput :sum.sync="yjTime.year" :dw="$t('baseinfo.time.year')" v-on:getNum="getYear"></incretInput>
+              <incretInput :sum.sync="yjTime.month" :dw="$t('baseinfo.time.month')" v-on:getNum="getMonth"></incretInput>
+              <incretInput :sum.sync="yjTime.day" :dw="$t('baseinfo.time.day')" v-on:getNum="getDay"></incretInput>
+
             </dd>
           </dl>
           <dl class="col_half">
             <dt>{{$t('baseinfo.cycle.title')}}</dt>
             <dd>
-              <h2>
-                <ul>
-                  <li>11{{$t('baseinfo.cycle.title')}}</li>
-                  <li>12{{$t('baseinfo.cycle.title')}}</li>
-                  <li>13{{$t('baseinfo.cycle.title')}}</li>
-                  <li class="show">14{{$t('baseinfo.cycle.title')}}</li>
-                  <li>15{{$t('baseinfo.cycle.title')}}</li>
-                  <li>16{{$t('baseinfo.cycle.title')}}</li>
-                  <li>17{{$t('baseinfo.cycle.title')}}</li>
-                </ul>
-                <i class="ylo_prev"></i>
-                <i class="ylo_next"></i>
-              </h2>
-            </dd>
+              <incretInput :sum.sync="yjWeekTime.day" :dw="$t('baseinfo.cycle.day')" v-on:getNum="getWeekDay"></incretInput>
+           </dd>
           </dl>
           <dl class="col_half">
             <dt>{{$t('baseinfo.rule.title')}}</dt>
             <dd>
-              <h2>
-                <ul>
-                  <li>11{{$t('baseinfo.rule.day')}}</li>
-                  <li>12{{$t('baseinfo.rule.day')}}</li>
-                  <li>13{{$t('baseinfo.rule.day')}}</li>
-                  <li class="show">14{{$t('baseinfo.rule.day')}}</li>
-                  <li>15{{$t('baseinfo.rule.day')}}</li>
-                  <li>16{{$t('baseinfo.rule.day')}}</li>
-                  <li>17{{$t('baseinfo.rule.day')}}</li>
-                </ul>
-                <i class="ylo_prev"></i>
-                <i class="ylo_next"></i>
-              </h2>
+              <incretInput :sum.sync="yjGetterTime.day" :dw="$t('baseinfo.rule.day')" v-on:getNum="getYjGetter"></incretInput>
+           </dd>
+          </dl>
+          <dl class="col_half">
+            <dt>{{$t('baseinfo.age.title')}}</dt>
+            <dd>
+              <incretInput :sum.sync="yjFirstTime.day" :dw="$t('baseinfo.age.day')" v-on:getNum="getYjAge"></incretInput>
             </dd>
           </dl>
           <dl class="col_half">
             <dt>{{$t('baseinfo.age.title')}}</dt>
             <dd>
-              <h2>
-                <ul>
-                  <li>11{{$t('baseinfo.age.day')}}</li>
-                  <li>12{{$t('baseinfo.age.day')}}</li>
-                  <li>13{{$t('baseinfo.age.day')}}</li>
-                  <li class="show">14{{$t('baseinfo.age.day')}}</li>
-                  <li>15{{$t('baseinfo.age.day')}}</li>
-                  <li>16{{$t('baseinfo.age.day')}}</li>
-                  <li>17{{$t('baseinfo.age.day')}}</li>
-                </ul>
-                <i class="ylo_prev"></i>
-                <i class="ylo_next"></i>
-              </h2>
+              <incretInput :sum.sync="yjWeekDay.day" :dw="$t('baseinfo.week.day')" v-on:getNum="getWeekTime"></incretInput>
             </dd>
           </dl>
         </div>
@@ -130,6 +73,7 @@
 <script>
 import leftMenu from '../leftMenu'
 import radio from '@/components/baseinfo/radio'
+import incretInput from '@/components/baseinfo/incretInput'
 import { get } from '@/libs/util'
 export default {
   name: 'bginfo',
@@ -222,21 +166,120 @@ export default {
           Active: false,
           label: this.$t('baseinfo.occupation.name11')
         }
-      ]
+      ],
+      time: {
+        year: null,
+        month: null,
+        day: null
+      },
+      yjTime: { //  月经lastTime
+        year: 2018,
+        month: 3,
+        day: 24
+      },
+      yjWeekTime: {
+        day: 24
+      },
+      yjGetterTime: {
+        day: 24
+      },
+      yjFirstTime: {
+        day: 24
+      },
+      yjWeekDay: {
+        day: 24
+      }
     }
   },
   components: {
     leftMenu,
-    radio
+    radio,
+    incretInput
+  },
+  watch: {
+    'yjTime.year' () {
+      if (this.yjTime.year > this.time.year) {
+        this.yjTime.year = this.time.year
+      }
+    },
+    'yjTime.month' () {
+      if (this.yjTime.month > 12) {
+        this.yjTime.month = 12
+      }
+    },
+    'yjTime.day' () {
+      let arr = [1, 3, 5, 7, 9]
+      let that = this
+      arr.forEach(item => {
+        if (item === that.yjTime.month) {
+          if (that.yjTime.day >= 31) {
+            debugger
+            that.yjTime.day = 31
+            return false
+          }
+        }
+      })
+      debugger
+      if (this.yjTime.month === 2) {
+        if (this.yjTime.year % 4) {
+          if (this.yjTime.day > 28) {
+            this.yjTime.day = 28
+            return false
+          }
+        } else {
+          if (this.yjTime.day > 29) {
+            this.yjTime.day = 29
+            return false
+          }
+        }
+      }
+      if (this.yjTime.day > 30) {
+        this.yjTime.day = 30
+        return false
+      }
+    }
   },
   mounted () {
     this.getMenuInfo()
+    this.getYearMonthDay()
   },
   methods: {
+    getYearMonthDay () {
+      let date = new Date()
+      this.time.year = date.getFullYear()
+      this.time.month = date.getMonth() + 1
+      this.time.day = date.getDate()
+      //  初始化日期
+      this.yjTime.year = this.time.year
+      this.yjTime.month = this.time.month
+      this.yjTime.day = this.time.day
+    },
     getMenuInfo () {
       let url = ''
       get(url, {}).then(res => {})
+    },
+    getYear (num) {
+      this.yjTime.year = num
+    },
+    getMonth (num) {
+      this.yjTime.month = num
+    },
+    getDay (num) {
+      this.yjTime.day = num
+    },
+    getWeekDay (num) {
+      this.yjWeekTime.day = num
+    },
+    getYjGetter (num) {
+      this.yjGetterTime.day = num
+    },
+    getYjAge (num) {
+      this.yjFirstTime.day = num
+    },
+    getWeekTime (num) {
+      this.yjWeekDay.day = num
     }
+
   }
 }
 </script>
